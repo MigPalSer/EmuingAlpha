a4105c5797ebd5264505fbb66985dee4c07789b5
mps <migpalser@gmail.com>
2021-12-06 06:46:59 +0100

    bucle de consola

---
+++ src/main/java/basicEmuing/TestReceptor.java
@@ -0,0 +1,25 @@
+package basicEmuing;
+
+import java.util.Scanner;
+
+public class TestReceptor {
+
+	public TestReceptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		BasicControlador bc=new BasicControlador();
+		
+		boolean activo=true;
+		
+		Scanner scn=new Scanner(System.in);
+		
+		do {
+			String comando=scn.nextLine();
+			if(!bc.ejecutaCMD(comando))activo=false;
+		} while (activo);
+	}
+
+}
